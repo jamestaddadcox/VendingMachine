06667886fd0c923fe960d177b740cdbe1d4364fd
James Tadd Adcox <jamestaddadcox@gmail.com>
2023-06-08 11:22:57 -0400

    main method scaffolding for VendingMachine

---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+public class VendingMachine {
+	public static void main(String[] args) {
+		VendingMachine vendingMachine = new VendingMachine();
+		vendingMachine.run();
+
+	}
+
+	public void run() {
+		// create inventory
+
+		// run menu
+	}
+}
